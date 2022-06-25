709cfc9a42d1bed17933c5d7656e428901ba1b5d
Fernando Luiz de Souza Vieira <32016610+740fernando@users.noreply.github.com>
2022-06-25 10:51:49 -0300

    Desafio 02 (#1)

---
+++ 02-desafio-santander/src/desafio02/Main.java
@@ -0,0 +1,16 @@
+package desafio02;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+
+	//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+	    int N = scan.nextInt();
+	    int M = scan.nextInt();
+	    
+	    System.out.println(N-M);
+	  }
+}
